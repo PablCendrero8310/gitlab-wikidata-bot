e80906525e43f658accc28838703a76f9139532e
Pablo <pabloagujeronegro2013@gmail.com>
2026-09-20 18:19:29 +0200

    fix: correct module

--- src/gitlab_wikidata_bot/__main__.py
+++ src/gitlab_wikidata_bot/__main__.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 import asyncio
 
-from github_wikidata_bot.main import main
+from gitlab_wikidata_bot.main import main
 
 if __name__ == "__main__":
     asyncio.run(main())
